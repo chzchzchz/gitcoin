a1c5f0774ee408dfe72489855aea661dea992f02
chz <chz@bingularity.org>
2015-10-29 01:58:23 -0700

    Welcome to gitcoin

---
+++ README.md
@@ -0,0 +1,27 @@
+# gitcoin #
+
+Now you too can be a _gitcoin_ cryptocurrency gorillionaire.
+
+## The git chain: an eternal golden braid ##
+
+The copyleft patent pending _gitcoin_ technology is bitcoin for git. A _gitcoin_ miner converts precious electricity to nonsense git commits. If the first 1+(lg(commits))/4 of the commit SHA are lucky, it's a new gitcoin and the world is all the more richer.
+
+_gitcoin_ is designed to be a naturally deflationary currency; the value of _gitcoin_ can only increase!
+Therefore, all rational agents must hoard gitcoin forever.
+To prevent irrational agents sabotaging the gitcoin network (cf., Byzantine economists problem), there is no way to spend/transfer gitcoins.
+
+## Usage ##
+
+Simply run **gitcoin.sh** from the gitcoin git repo. Once you mine gitcoin, submit a "github" "pull request" and it'll be official! It's that easy!
+
+## People are Talking about gitcoin! ##
+
+"how do i cash in my _gitcoin_" -- trevis
+
+"did you make millions from _gitcoin_ yet" -- steef
+
+"now that's a blue ocean strategy people will put money on" -- crumb
+
+"gotta make extra $$$ so I can lead the seed round for _gitcoin_" -- job
+
+"i still say there are massively inherint problems with introducing a new type of currency, especially one that's only available online.. (in response to the _gitcoin_ stuff earlier btw). ah well." -- matt!!!
---
+++ gitcoin.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Welcome to gitcoin.
+
+
+title="$1"
+if [ -z "$title" ]; then
+	title="gitcoin: "`whoami`
+fi
+
+WALLET="$HOME/.gitcoin"
+mkdir -p $WALLET
+if [ ! -e "$WALLET"/id_rsa.pub ]; then
+	ssh-keygen <<<"$WALLET/id_rsa"
+fi
+
+x=`git log 2>&1 | head -n1 | grep fatal | cut -f1 -d':'`
+if [ ! -z "$x" ]; then
+	rm -f gitchain.dat
+	git init
+	git add *
+	git commit -m "Welcome to gitcoin"
+	git checkout --orphan gitbux
+	git rm -f *
+	touch gitchain.dat
+	git add gitchain.dat
+	git commit -m "Mint the first gitcoin"
+fi
+
+git checkout gitbux >/dev/null 2>&1
+if [ "$?" != "0" ]; then
+	echo no gitbux branch huh
+	exit 1
+fi
+
+commits=`git rev-list HEAD --count`
+n=`bc -l <<< "1+(l($commits)/l(2))/4" |  cut -f1 -d'.'`
+x="0"
+oldhash=`git log | head -n1 | grep commit | cut -f2 -d' '`
+while [ 1 ]; do
+	git checkout -b gitbux-work >/dev/null 2>&1
+	cat $WALLET/id_rsa.pub >>gitchain.dat
+	x=`expr $x + 1`
+	echo "$x" >>gitchain.dat
+	git add gitchain.dat >/dev/null  2>&1
+	git commit -m "$title" >/dev/null 2>&1
+	newhash=`git log | head -n1 | grep commit | cut -f2 -d' '`
+	t=`echo $oldhash $newhash | awk '{ print substr($1,0,'$n') " == " substr($2,0,'$n'); }'`
+	if [ $t ]; then
+		echo "Congratulations you have mined GITCOIN!"
+		echo $t
+		echo $x
+		git checkout gitbux
+		git merge gitbux-work
+		git checkout master
+		exit
+	fi
+	git checkout gitbux >/dev/null 2>&1
+	git branch -D gitbux-work >/dev/null 2>&1
+done
+
